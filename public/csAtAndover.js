b687e4937553896dc052cea09f6f4cb24fb55e13
emma2357 <capaldi.ei@gmail.com>
2024-05-13 13:42:40 -0400

    changes transfered over from glitch

--- public/csAtAndover.js
+++ public/csAtAndover.js
@@ -3,18 +3,34 @@
 // when any skill is selected, the hidden text is revealed and the 
 // "ExpandMore" icon is replaced with the "explandLess" icon. 
 // When the skill is clicked again, the reverse occurs. 
+// function toggleText(button) {
+//   var hiddenText = button.nextElementSibling;
+//   var expandMoreIcon = button.querySelector('.expandMore');
+//   var expandLessIcon = button.querySelector('.expandLess');
+
+//   if (hiddenText.classList.contains('hidden-text')) {
+//     hiddenText.classList.remove('hidden-text');
+//     expandMoreIcon.style.display = 'none';
+//     expandLessIcon.style.display = 'inline-block';
+//   } 
+//   else {
+//     hiddenText.classList.add('hidden-text');
+//     expandMoreIcon.style.display = 'inline-block';
+//     expandLessIcon.style.display = 'none';
+//   }
+// }
+
 function toggleText(button) {
   var hiddenText = button.nextElementSibling;
   var expandMoreIcon = button.querySelector('.expandMore');
   var expandLessIcon = button.querySelector('.expandLess');
 
-  if (hiddenText.classList.contains('hidden-text')) {
-    hiddenText.classList.remove('hidden-text');
+  if (!hiddenText.classList.contains('visible')) {
+    hiddenText.classList.add('visible');
     expandMoreIcon.style.display = 'none';
     expandLessIcon.style.display = 'inline-block';
-  } 
-  else {
-    hiddenText.classList.add('hidden-text');
+  } else {
+    hiddenText.classList.remove('visible');
     expandMoreIcon.style.display = 'inline-block';
     expandLessIcon.style.display = 'none';
   }
