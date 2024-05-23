90ea4a9f6c91189662d10eb730b5a608e2e9479a
Yngve135 <magnus@ejulin.com>
2024-05-23 14:38:25 -0400

    added uploading functionality

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 
+const handler = require('./handler/handler');
+
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 app.use(express.urlencoded({extended: false}))
 //initializing passport and adding its middleware
 app.use(passport.initialize())
@@ -35,49 +41,15 @@ app.use(session({
 //manages session data
 app.use(passport.session())
 
-
-// IN PROGRESS: Uploading, parsing, and saving excel files -----------
-
-// a lot of this will go into middleware!
-
-//const multer = require('multer');
-//const xlsx = require('xlsx');
-//const upload = multer({ dest: 'uploads/' });
-
-// // uploading excel files template code
-// app.post('/upload', upload.single('file'), async (req, res) => {
-//     const workbook = xlsx.readFile(req.file.path);
-//     const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-//     const data = xlsx.utils.sheet_to_json(worksheet, { header: 1 });
-
-//     try {
-//         for (const item of data) {
-//             await connection.query(
-//                 // logic goes here!
-
-//                 // EXAMPLE CODE -----
-//                 // `INSERT INTO excel_data (task, subtask, uom_labour, labour_quantity, labour_total, uom_material, material_quantity, material_total, complete, value)
-//                 // VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-//                 // [
-//                 //     item.Task,
-//                 //     item.Subtask,
-//                 //     item['UoM Labour'],
-//                 //     item['Labour Quantity'],
-//                 //     item['Labour Total'],
-//                 //     item['UoM Material'],
-//                 //     item['Material Quantity'],
-//                 //     item['Material Total'],
-//                 //     item.Complete,
-//                 //     item.Value,
-//                 // ]
-//                 // -------------
-//             );
-//         }
-//     } catch (err) {
-//         console.error('Error inserting data:', err);
-//         res.status(500).json({ error: 'Error inserting data into the database.' });
-//     } 
-// });
-// ------------------------
+app.post("/api/upload", upload.single('file'), async (req, res) => {
+    EoLs = await handler.parseData(req.file.buffer);
+    if (EoLs != -99) {
+        res.send(EoLs);
+        console.log("wrong thingy");
+    } else {
+        console.log("error thrown");
+    }
+    console.log(EoLs);
+})
 
 app.listen(process.env.PORT || 3000, () => console.log('server listening'));
