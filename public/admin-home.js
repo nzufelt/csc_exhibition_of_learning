b9ed1ee4e248d7b5b68b9ec16e58de9d6c80694b
emma2357 <capaldi.ei@gmail.com>
2024-05-28 17:41:38 -0400

    added admin home code template

---
+++ public/admin-home.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // toggle the visibility of the search pop-up
+    var searchExhibitionsButton = document.getElementById('search_button');
+    searchExhibitionsButton.addEventListener('click', function() {
+      toggleSearchPopUp();
+    });
+  
+    function toggleSearchPopUp() {
+      var searchPopUp = document.querySelector('.search_pop_up');
+      if (searchPopUp.style.display === 'block') {
+        searchPopUp.style.display = 'none';
+      } else {
+        searchPopUp.style.display = 'block';
+      }
+    }
+  
+    // handle tag button clicks
+    const tagButtons = document.querySelectorAll('.tag');
+    const tagInfoPanel = document.getElementById('tag_info_panel');
+    const tagInfoTitle = document.getElementById('tag_info_title');
+    const closeTagInfoButton = document.getElementById('close_button');
+  
+    tagButtons.forEach(button => {
+      button.addEventListener('click', function() {
+        tagInfoTitle.textContent = this.textContent;
+        tagInfoPanel.style.display = 'block';
+      });
+    });
+  
+    closeTagInfoButton.addEventListener('click', function() {
+      tagInfoPanel.style.display = 'none';
+    });
+  
+    // toggle the visibility of the new tag pop-up
+    var makeNewTagButton = document.getElementById('make_new_tag_button');
+    var newTagPanel = document.getElementById('new_tag_panel');
+    var closeNewTagButton = document.getElementById('new_tag_close_button');
+  
+    makeNewTagButton.addEventListener('click', function() {
+      newTagPanel.style.display = 'block';
+    });
+  
+    closeNewTagButton.addEventListener('click', function() {
+      newTagPanel.style.display = 'none';
+    });
+  
+    // filter tag buttons based on search
+    function tag_search() {
+      const searchbox = document.getElementById("tag_search_bar").value.toUpperCase();
+      const allTags = document.querySelectorAll('.all_tags .tag'); // select all tag buttons within the .all_tags container
+  
+      allTags.forEach(button => {
+        const buttonText = button.textContent.toUpperCase();
+  
+        if (buttonText.includes(searchbox)) {
+          button.style.display = ""; // show the tag button if it matches the search
+        } else {
+          button.style.display = "none"; // hide the tag button if it doesn't match the search
+        }
+      });
+    }
+  
+    // attach the search function to the input field
+    document.getElementById('tag_search_bar').addEventListener('keyup', tag_search);
+  });
