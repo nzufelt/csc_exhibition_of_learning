a3db4403ea5d6c5397f612dee522fb2ddb3780db
emma2357 <capaldi.ei@gmail.com>
2024-05-28 17:27:07 -0400

    quick fix to uploading button to load admin home

--- routes/route_index.js
+++ routes/route_index.js
@@ -12,6 +12,18 @@ const bcrypt = require('bcrypt')
 const checkAuthentication = require('../authentication')
 const initializePassport = require('../passport-config')
 
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '/uploads')
+    },
+    filename: function (req, file, cb) {
+        //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.originalname)
+      }
+})
+const upload = multer({ storage })
+
 //for testing
 // const users = []
 // users.push({
