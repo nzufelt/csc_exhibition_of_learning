d9f0e2621c4bb508c5891d33a57cb00473237008
emma2357 <capaldi.ei@gmail.com>
2024-05-28 21:24:45 -0400

    cleaned up route_index file

--- routes/route_index.js
+++ routes/route_index.js
@@ -173,15 +173,8 @@ router.get('/health', async(req, res) => {
     res.send("hello world!");
 });
 
-// POSTs FOR ADMIN CREATING/EDITING
-
-// CREATING ------
-
-// COURSES
-router.get('/create-course-form', async function(req, res){
-    res.render("admin_creation_forms/create_course_form");
-});
-
+// Posts for admin creating/editing/deleting data
+// post to create course
 router.post('/create-course', async function(req, res){
     const course_number = req.body.course_number;
     const course_name = req.body.course_name;
@@ -190,14 +183,10 @@ router.post('/create-course', async function(req, res){
 
     await courseController.createCourse(course_number, course_name, course_description, course_level);
 
-    res.send("Course Submitted Successfully!" + course_number + course_name + course_description + course_level);
+    res.send("Course Created Successfully!");
 });
 
-// SKILLS
-router.get('/create-skill-form', async function(req, res){
-    res.render("admin_creation_forms/create_skill_form");
-});
-
+// post to create skill
 router.post('/create-skill', async function(req, res){
     const skill_name = req.body.skill_name;
     const skill_description = req.body.skill_description;
@@ -205,14 +194,10 @@ router.post('/create-skill', async function(req, res){
 
     await skillController.createSkill(skill_name, skill_description, throughline);
 
-    res.send("Skill Submitted Successfully! (" + skill_name + skill_description + throughline + ")");
+    res.send("Skill Created Successfully!");
 });
 
-// ADMINS
-router.get('/create-admin-form', async function(req, res){
-    res.render("admin_creation_forms/create_admin_form");
-});
-
+// post to create admin
 router.post('/create-admin', async function(req, res){
     const email = req.body.email;
     const password_unencrypted = req.body.password;
@@ -222,27 +207,10 @@ router.post('/create-admin', async function(req, res){
 
     await adminController.createAdmin(email, password_encrypted, name, bio);
 
-    res.send("Admin Created Successfully!", email, password_encrypted, name, bio);
-});
-// ----------------
-
-// EDITING --------------
-// COURSES
-router.get('/edit-course-form', async function(req, res){
-    // PULL THE DATA OR RECEIVE THE DATA HERE (this will likely be subheader in another form so it's all good)
-    const exampleData = {
-        course_id : "10001",
-        course_number : "CSC501",
-        course_name : "Machine Learning",
-        course_description : "In this course ...",
-        course_level : "Advanced"
-    }
-    res.render("admin_edit_forms/edit_course_form", 
-    {
-        course : exampleData
-    });
+    res.send("Admin Created Successfully!");
 });
 
+// post to skill course
 router.post('/edit-course', async function(req, res){
     const course_number = req.body.course_number;
     const course_name = req.body.course_name;
@@ -252,23 +220,10 @@ router.post('/edit-course', async function(req, res){
 
     await courseController.editCourse(course_id, course_number, course_name, course_description, course_level);
 
-    res.send("Course Edited Successfully!" + course_id + course_number + course_name + course_description + course_level);
+    res.send("Course Edited Successfully!");
 });
 
-// SKILLS
-router.get('/edit-skill-form', async function(req, res){
-    const exampleData = {
-        skill_id : "102",
-        skill_name : "Refactoring Code",
-        skill_description : "This skill ...",
-        throughline : "Working With Code"
-    }
-
-    res.render("admin_edit_forms/edit_skill_form", {
-        skill : exampleData
-    });
-});
-
+// post to edit skill
 router.post('/edit-skill', async function(req, res){
     const skill_id = req.body.skill_id;
     const skill_name = req.body.skill_name;
@@ -279,18 +234,10 @@ router.post('/edit-skill', async function(req, res){
 
     await skillController.editSkill(skill_id, skill_name, skill_description, throughline);
 
-    res.send("Skill Edited Successfully! (" + skill_id + skill_name + skill_description + throughline + ")");
+    res.send("Skill Edited Successfully!");
 });
 
-// USERS
-router.get('/edit-user-form', async function(req, res){
-    const exampleData = {user_id: 101, email: 'ecapaldi@andover.edu', first_name: "Emma", last_name: "Capaldi", graduation_year: 2025, bio: "In my free time ..."}
-
-    res.render("admin_edit_forms/edit_user_form", {
-        user : exampleData
-    });
-});
-
+// post to edit user/student
 router.post('/edit-user', async function(req, res){
     const user_id = req.body.user_id;
     const email = req.body.email;
@@ -301,23 +248,10 @@ router.post('/edit-user', async function(req, res){
 
     await userController.editUser(user_id, email, first_name, last_name, parseInt(graduation_year), bio);
 
-    res.send("User Edited Successfully!", user_id, email, first_name, last_name, parseInt(graduation_year), bio);
+    res.send("User Edited Successfully!");
 });
 
-// SKILLS
-router.get('/edit-skill-form', async function(req, res){
-    const exampleData = {
-        skill_id : "102",
-        skill_name : "Refactoring Code",
-        skill_description : "This skill ...",
-        throughline : "Working With Code"
-    }
-
-    res.render("admin_edit_forms/edit_skill_form", {
-        skill : exampleData
-    });
-});
-
+// post to edit skill
 router.post('/edit-skill', async function(req, res){
     const skill_id = req.body.skill_id;
     const skill_name = req.body.skill_name;
@@ -328,19 +262,10 @@ router.post('/edit-skill', async function(req, res){
 
     await skillController.editSkill(skill_id, skill_name, skill_description, throughline);
 
-    res.send("Skill Edited Successfully! (" + skill_id + skill_name + skill_description + throughline + ")");
+    res.send("Skill Edited Successfully!");
 });
 
-// ADMINS
-router.get('/edit-admin-form', async function(req, res){
-
-    const exampleData = {admin_id: 103, email: 'nzufelt@andover.edu', password: "$2a$12$VvnJbkxgor1ktfpHvQD0g.k6bKAh7ZTZOoQky1k9TggsgxVo9ahIi", name: "Nick Zufelt", bio: "I love to teach..."}
-    
-    res.render("admin_edit_forms/edit_admin_form", {
-        admin : exampleData
-    });
-});
-
+// post to edit admin/teacher
 router.post('/edit-admin', async function(req, res){
     const admin_id = req.body.admin_id;
     const email = req.body.email;
@@ -351,10 +276,10 @@ router.post('/edit-admin', async function(req, res){
 
     await adminController.editAdmin(admin_id, email, password_encrypted, name, bio);
 
-    res.send("Admin Edited Successfully!", admin_id, email, password_encrypted, name, bio);
+    res.send("Admin Edited Successfully!");
 });
 
-// EXHIBITIONS
+// post to edit exhibitions (IN PROGRESS)
 router.get('/edit-exhibition-form', async function(req, res){
     const exampleData = {exhibition_id: 1001, user_id_ref: 102, class_id_ref: 101, display_on_home_page: true, description: "In this video...",video_html_code:'<iframe id="kaltura_player" src="https://cdnapisec.kaltura.com/p/1188822/sp/118882200/embedIframeJs/uiconf_id/25697092/partner_id/1188822?iframeembed=true&playerId=kaltura_player&entry_id=1_ofiuys05&flashvars[streamerType]=auto&amp;flashvars[localizationCode]=en&amp;flashvars[hotspots.plugin]=1&amp;flashvars[sideBarContainer.plugin]=true&amp;flashvars[sideBarContainer.position]=left&amp;flashvars[sideBarContainer.clickToClose]=true&amp;flashvars[chapters.plugin]=true&amp;flashvars[chapters.layout]=vertical&amp;flashvars[chapters.thumbnailRotator]=false&amp;flashvars[streamSelector.plugin]=true&amp;flashvars[EmbedPlayer.SpinnerTarget]=videoHolder&amp;flashvars[dualScreen.plugin]=true&amp;flashvars[Kaltura.addCrossoriginToIframe]=true&amp;&wid=1_4mlte2vv" width="400" height="285" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" sandbox="allow-downloads allow-forms allow-same-origin allow-scripts allow-top-navigation allow-pointer-lock allow-popups allow-modals allow-orientation-lock allow-popups-to-escape-sandbox allow-presentation allow-top-navigation-by-user-activation" frameborder="0" title="Screen Recording 2024-04-23 at 2.18.06 PM"></iframe>'}
     
